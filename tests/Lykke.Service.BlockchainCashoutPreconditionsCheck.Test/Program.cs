17233c6d5f768a74854ea93abafd0c5480b91d3c
AleksandrBelousov <tortyt1@gmail.com>
2018-01-18 20:17:59 +0400

    commented lines

--- tests/Lykke.Service.BlockchainCashoutPreconditionsCheck.Test/Program.cs
+++ tests/Lykke.Service.BlockchainCashoutPreconditionsCheck.Test/Program.cs
@@ -7,14 +7,14 @@ namespace Lykke.Service.BlockchainCashoutPreconditionsCheck.Test
     {
         static void Main(string[] args)
         {
-            Task.Delay(10000).Wait();
-            var client = new Lykke.Service.BlockchainCashoutPreconditionsCheck.Client.BlockchainCashoutPreconditionsCheckClient("http://localhost:5000",null);
-            var (isAllowed, listOfErrors) = client.ValidateCashoutAsync(new Client.Models.CashoutValidateModel()
-            {
-                AssetId = "62c04960-4015-4945-bb7e-8e4a193b3653",
-                DestinationAddress = "0x406561f72e25ab41200fa3d52badc5a21",
-                Amount = 0
-            }).Result;
+            //Task.Delay(10000).Wait();
+            //var client = new Lykke.Service.BlockchainCashoutPreconditionsCheck.Client.BlockchainCashoutPreconditionsCheckClient("http://localhost:5000",null);
+            //var (isAllowed, listOfErrors) = client.ValidateCashoutAsync(new Client.Models.CashoutValidateModel()
+            //{
+            //    AssetId = "62c04960-4015-4945-bb7e-8e4a193b3653",
+            //    DestinationAddress = "0x406561f72e25ab41200fa3d52badc5a21",
+            //    Amount = 0
+            //}).Result;
 
             Console.ReadLine();
         }
