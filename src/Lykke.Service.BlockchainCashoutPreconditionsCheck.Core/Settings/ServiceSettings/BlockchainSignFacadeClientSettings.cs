790efbbcc8321302375311407097af90e6b1b67e
some-dev <some-dev@gmail.com>
2018-04-23 17:00:43 +0300

    remove unused settings class

--- src/Lykke.Service.BlockchainCashoutPreconditionsCheck.Core/Settings/ServiceSettings/BlockchainSignFacadeClientSettings.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Lykke.Service.BlockchainCashoutPreconditionsCheck.Core.Settings.ServiceSettings
-{
-    public class BlockchainSignFacadeClientSettings
-    {
-        public string ServiceUrl { get; set; }
-    }
-}
