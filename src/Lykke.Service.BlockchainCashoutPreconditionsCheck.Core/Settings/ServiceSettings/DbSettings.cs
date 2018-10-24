ef4e6a096775d7747ca4c9f3b6b10852b23969a8
Dmitry Samodov <samodov.dmitry@gmail.com>
2018-10-24 13:01:40 +0300

    use sdk and new logging

--- src/Lykke.Service.BlockchainCashoutPreconditionsCheck.Core/Settings/ServiceSettings/DbSettings.cs
+++ src/Lykke.Service.BlockchainCashoutPreconditionsCheck.Core/Settings/ServiceSettings/DbSettings.cs
@@ -1,8 +1,12 @@
-﻿namespace Lykke.Service.BlockchainCashoutPreconditionsCheck.Core.Settings.ServiceSettings
+﻿using Lykke.SettingsReader.Attributes;
+
+namespace Lykke.Service.BlockchainCashoutPreconditionsCheck.Core.Settings.ServiceSettings
 {
     public class DbSettings
     {
+        [AzureTableCheck]
         public string DataConnString { get; set; }
+        [AzureTableCheck]
         public string LogsConnString { get; set; }
     }
 }
